8f63a3036c135a4408d7040bcc7876395502d8a4
masac <msktjm98@gmail.com>
2023-03-23 02:15:22 +0900

    chakra uiの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { Input, Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/layout";
+
 const App = () => {
   return (
-    <>
+    <ChakraProvider>
       <h1>題名</h1>
-      <div>
-        入力するところ
+      <Box>
+        <Input placeholder="件名" />
+        <Input placeholder="内容" />
         <br />
-        <button>追加</button>
-      </div>
-      <div>
+        <Button>追加ボタン</Button>
+      </Box>
+      <Box>
         <h2>一覧</h2>
         <dl>
-          <dt>件名</dt>
-          <dd>内容</dd>
-          <button>編集</button>
-          <button>削除</button>
+          <dt>件名1</dt>
+          <dd>内容1</dd>
+          <Button>編集ボタン</Button>
+          編集ボタンを押すとモーダルウィンドウが出てくるようにしたい
+          <Button>削除ボタン</Button>
+          削除したtodo一覧のページを作りたい
         </dl>
-      </div>
-    </>
+        <dl>
+          <dt>件名2</dt>
+          <dd>内容2</dd>
+          <Button>編集ボタン</Button>
+          <Button>削除ボタン</Button>
+        </dl>
+      </Box>
+    </ChakraProvider>
   );
 };
 
