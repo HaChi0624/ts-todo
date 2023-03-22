7b23ae26e8ad93aee4d7a1245bbcdabe8434c97b
masac <msktjm98@gmail.com>
2023-03-23 01:39:04 +0900

    ひな形

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ const App = () => {
   return (
     <>
       <h1>題名</h1>
-      <div>入力するところ</div>
+      <div>
+        入力するところ
+        <br />
+        <button>追加</button>
+      </div>
       <div>
         <h2>一覧</h2>
         <dl>
-          <dt></dt>
-          <dd></dd>
+          <dt>件名</dt>
+          <dd>内容</dd>
+          <button>編集</button>
+          <button>削除</button>
         </dl>
       </div>
     </>
